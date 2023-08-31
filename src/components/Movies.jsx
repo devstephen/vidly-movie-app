79479877b95c44ce4500c91d248d8b8daf478d2b
Fatolu Stephen <smjssp@gmail.com>
2023-08-31 13:31:12 +0100

    Add search component and logic

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -7,6 +7,7 @@ import { getMovies } from '../services/fakeMovieService'
 import { getGenres } from '../services/fakeGenreService'
 import { paginate } from '../utils/paginate'
 import _ from 'lodash'
+import SearchBox from './common/SearchBox'
 
 class Movies extends Component {
   state = {
@@ -14,6 +15,8 @@ class Movies extends Component {
     genres: [],
     currentPage: 1,
     pageSize: 4,
+    selectedGenre: null,
+    searchQuery: '',
     sortColumn: { path: 'title', order: 'asc' },
   }
 
@@ -41,7 +44,11 @@ class Movies extends Component {
   }
 
   handleGenreSelect = (genre) => {
-    this.setState({ selectedGenre: genre, currentPage: 1 })
+    this.setState({ selectedGenre: genre, searchQuery: '', currentPage: 1 })
+  }
+
+  handleSearch = (query) => {
+    this.setState({ searchQuery: query, selectedGenre: null, currentPage: 1 })
   }
 
   handleSort = (sortColumn) => {
@@ -55,12 +62,16 @@ class Movies extends Component {
       sortColumn,
       selectedGenre,
       movies: allMovies,
+      searchQuery,
     } = this.state
 
-    const filtered =
-      selectedGenre && selectedGenre._id
-        ? allMovies.filter((m) => m.genre._id === selectedGenre._id)
-        : allMovies
+    let filtered = allMovies
+    if (searchQuery)
+      filtered = allMovies.filter((m) =>
+        m.title.toLowerCase().startsWith(searchQuery.toLowerCase())
+      )
+    else if (selectedGenre && selectedGenre._id)
+      filtered = allMovies.filter((m) => m.genre._id === selectedGenre._id)
 
     const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order])
 
@@ -93,6 +104,10 @@ class Movies extends Component {
             </Link>
           </div>
           <p>Showing {totalCount} movies in the database.</p>
+          <SearchBox
+            value={this.state.searchQuery}
+            onChange={this.handleSearch}
+          />
           <MoviesTable
             movies={movies}
             sortColumn={sortColumn}
